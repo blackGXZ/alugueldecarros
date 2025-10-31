d05f6c5de5017db5a233fa0ffd499aac2516f56b
Nubia Monteiro <nubia01_@outlook.com>
2025-10-30 21:25:05 -0300

    Criado as classes

---
+++ src/main/java/entities/Invoice.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class Invoice {
+    private  Double basicPayment;
+    private  Double tax;
+
+    //Constructor
+    public Invoice() {
+    }
+    public Invoice(Double basicPayment, Double tax) {
+        this.basicPayment = basicPayment;
+        this.tax = tax;
+    }
+    //Getter and Setter
+    public Double getBasicPayment() {
+        return basicPayment;
+    }
+
+    public void setBasicPayment(Double basicPayment) {
+        this.basicPayment = basicPayment;
+    }
+
+    public Double getTax() {
+        return tax;
+    }
+
+    public void setTax(Double tax) {
+        this.tax = tax;
+    }
+    //Method
+    public Double getTotalPyament(){
+        return getBasicPayment() - getTax();
+    }
+}
---
+++ src/main/java/entities/Vehicle.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Vehicle {
+
+    private String model;
+
+    //Construtor
+    public Vehicle() {
+    }
+    public Vehicle(String model) {
+        this.model = model;
+    }
+    //Getter and Setter
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
